5e706a6185ec6c96feea5652686398129f2e5af2
smokett <47299637+smokett@users.noreply.github.com>
2019-03-07 23:22:51 +0000

    Add files via upload

--- chromeExtension/popup.js
+++ chromeExtension/popup.js
@@ -5,23 +5,35 @@ navigator.geolocation.getCurrentPosition(function(pos,error){
         latitude=pos.coords.latitude;                             
         longitude=pos.coords.longitude;
         var geocodingAPI = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+latitude+","+longitude+"&key=AIzaSyCUe-myHsErw9OjBwB7mIqlo4FzYX-qzkw";
+        var city = "";
         $.getJSON(geocodingAPI, function (json) {
             if (json.status == "OK") {
                 //Check result 0
                 var result = json.results[0];
                 //look for locality tag and administrative_area_level_1
-                var city = "";
-                var state = "";
+                city = "";
                 for (var i = 0, len = result.address_components.length; i < len; i++) {
                     var ac = result.address_components[i];
-                   if (ac.types.indexOf("administrative_area_level_1") >= 0) state = ac.short_name;
+                   if (ac.types.indexOf("administrative_area_level_1") >= 0) city = ac.short_name;
                 }
-                if (state != '') {
-                    console.log("Hello to you out there in "+state );
-                    document.getElementById("showPosition").innerHTML="your location is: "+latitude+","+longitude+"  That's "+state;
+                if (city != '') {
+                    console.log("Hello to you out there in "+city );
+                    document.getElementById("showPosition").innerHTML="your location is: "+latitude+","+longitude+"  That's "+city;
                 }
             }
         
+        });
+        var dataurl = chrome.extension.getURL("data/data.json");
+        $.getJSON(dataurl,function(json) {
+            for(var i = 0; i < json.records.length ; i++)
+            {
+                var result = json.records[i];
+                if(result.city == city)
+                var time = "";
+                document.getElementById("showWarnings").innerHTML+="time: " +result.time +"\n"+"level: " +result.level+"\n"+"details: " +result.details;
+            }
+            
+
         });
 });
 
