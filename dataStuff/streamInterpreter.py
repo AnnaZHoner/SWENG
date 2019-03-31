118af8b3daf72409af0a92e350b1bb081441aaf1
Ernest Kuznecovs <34303435+ErnestKz@users.noreply.github.com>
2019-04-01 00:18:22 +0100

    brainstormed a plan in detail and began on accepting input from the input strewam

---
+++ dataStuff/streamInterpreter.py
@@ -0,0 +1,14 @@
+streamFile = "sampleStreamFile.txt"
+
+# Used to read the oldest line of the file (which is the last one.)
+readFile = open(streamFile, "r")
+# Used to delete a line once its read.
+writeFile = open(streamFile, "w")
+
+
+while true:
+    readfile.read
+    
+    
+    
+#writeFile.flush()
