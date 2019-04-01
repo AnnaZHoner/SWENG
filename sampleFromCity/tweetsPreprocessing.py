613689627593d3652b793ab045c1ab85bbf74671
Ernest Kuznecovs <34303435+ErnestKz@users.noreply.github.com>
2019-04-01 14:21:59 +0100

    added code to precess twitter tweets

--- sampleFromCity/tweetsPreprocessing.py
+++ sampleFromCity/tweetsPreprocessing.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
-tweets = pd.read_json("SF_2014-03-10.json")
+tweets = pd.read_json("SF_2014-08-24.json")
+
+# 13304 to 12572
+# 3 minutes worth of tweets after an earthquake
+relevantTweets = tweets.iloc[12572:13304 :]
+file = open("sampleTweets.txt","a") 
+for index, row in relevantTweets.iterrows():
+     file.write( str(str(row['timestamp']).encode("utf-8")) + " " + str(str(row['text']).encode("utf-8")))
+     file.write("\n")
+     
+file.close()
