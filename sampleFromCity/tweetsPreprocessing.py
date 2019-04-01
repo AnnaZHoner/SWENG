4c60051d4e2abbc376b2da9f867db3fcb0a21bfd
Ernest Kuznecovs <34303435+ErnestKz@users.noreply.github.com>
2019-04-01 23:56:31 +0100

    added presentation code

--- sampleFromCity/tweetsPreprocessing.py
+++ sampleFromCity/tweetsPreprocessing.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import json
 
+import time
+
 tweets = pd.read_json("SF_2014-08-24.json")
 
 # 13304 to 12572
 # 3 minutes worth of tweets after an earthquake
 relevantTweets = tweets.iloc[12572:13304]
-
+#13279
+relevantTweets.drop(13279, inplace=True)
 # Process tweets
 # Need to pass a thread/object info 
 # The object keeps stats on the state of its location(each object is based on location)
@@ -18,38 +21,37 @@ numberNotContainKeyword = 1
 tweets = []
 numberOfNewestTweetsToLookAt = 30
 # if there are 3 tweets containing "earthquake" to 1 not containing, it will trigger the alert
-alarmThreshold = 0.5
+alarmThreshold = 28
 
 alerted = False
 
-for index, row in relevantTweets.iterrows():
+for index, row in relevantTweets[::-1].iterrows():
+    time.sleep(0.5)
     tweets.append(row['text'])
-    if 'earthquake' in row['text']:
+    if 'earthquake' or 'quake' in row['text']:
         numberContainKeyword = numberContainKeyword + 1
     else:
         numberNotContainKeyword = numberNotContainKeyword + 1
-    
+    print("Tweet: " + row['text'])
+    print("Analysing " + str(numberContainKeyword + numberNotContainKeyword) + " tweets")
+    print(str(numberContainKeyword) + " tweets related to earthquake")
+    print(str(numberNotContainKeyword) + " tweets not related to earthquake")
+    print("Earthquake Probability: " + str((numberContainKeyword / numberNotContainKeyword) / alarmThreshold))
+    print()
 # pops off 31'st tweet and adjusts the counter
-    if len(tweets) >= 30 :
-        if 'earthquake' in tweets.pop():
+    if len(tweets) >= 29 :
+        if 'earthquake' or 'quake' in tweets.pop():
             numberContainKeyword = numberContainKeyword - 1
         else:
             numberNotContainKeyword = numberNotContainKeyword - 1
             
-    if (not alerted) and (numberContainKeyword / numberNotContainKeyword >= alarmThreshold and (numberContainKeyword + numberNotContainKeyword > 29)):
+    if (not alerted) and (numberContainKeyword / numberNotContainKeyword >= alarmThreshold and (numberContainKeyword + numberNotContainKeyword >= 29)):
         # put info in json file 
         # need to make a dictionary with the stats
-        alertData = {"location": location, "distress ratio": numberContainKeyword / numberNotContainKeyword, "tweets": tweets }
+        #alertData = {"location": location, "distress ratio": numberContainKeyword / numberNotContainKeyword, "tweets": tweets }
         alerted = True
-        with open('alerts.json', 'a') as outfile:
-            json.dump(alertData, outfile)
-     
-
-"""
-file = open("sampleTweets.txt","a") 
-for index, row in relevantTweets.iterrows():
-     file.write( str(str(row['timestamp']).encode("utf-8")) + " " + str(str(row['text']).encode("utf-8")))
-     file.write("\n")
-     
-file.close()
-"""
+        print("EARTHQUAKE IN SAN FRANSISCO")
+        break;
+        #with open('alerts.json', 'a') as outfile:
+            #json.dump(alertData, outfile)
+        
