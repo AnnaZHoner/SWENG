8babb59216ba8316da819cf1ea523647e22facea
rwejiv <carlzzzlk@gmail.com>
2019-04-01 15:25:04 +0100

    filters san francisco tweets

--- stream-listener/twitter-stream-listener.py
+++ stream-listener/twitter-stream-listener.py
@@ -5,21 +5,24 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "1084208607120580609-WoZXnSwpiLVW5jYQVOM9ZlifiVVca7"
+access_token_secret = "goiceTphkHIAxqbB9o16SRTFL3gDYrphbU9QP5FnxD0Uy"
+consumer_key = "04FC3oJXzYsJRPuTG4xTXh7rp"
+consumer_secret = "fOFp5SRPAXcNtIu6EfzDOulV1ZfWhmjtnb4VaaT8Nml2Q9VSPl"
+
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
+    #cities = ["New York", "London"]
     def __init__(self, api=None):
         super(StdOutListener, self).__init__()
         self.num_tweets = 0
-        self.file = open("tweets.json", "w")
+        self.file = open("tweets_word.json", "w")
     def on_data(self, data):
         tweet = json.loads(data)
         if (not tweet["retweeted"]) and ('RT @' not in tweet["text"]):
-            self.file.write(json.dumps(tweet["user"]["location"]) + ',' + json.dumps(tweet["text"]) + '\n')
+            #if ("New York" in (tweet['user']['location'])):
+            self.file.write(json.dumps(tweet['user']['location']) + ',' + json.dumps(tweet['text']) + '\n')
         return True
 
     def on_error(self, status):
@@ -34,4 +37,5 @@ if __name__ == '__main__':
     stream = Stream(auth, l, tweet_mode = "extended", lang = 'en')
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['quake', 'earthquake', 'shaking', 'ground'])
+    #stream.filter(track=['quake', 'earthquake', 'temblor', 'tremor', 'aftershock', 'magnitude', 'seismic', 'seismology', 'epicentre'])
+    stream.filter(locations=[-122.75,36.8,-121.75,37.8])
