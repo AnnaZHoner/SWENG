3e0dc28e096568eae8a04515c64e092d97e3c3ea
Ernest Kuznecovs <ErnestKz@users.noreply.github.com>
2019-04-08 19:09:36 +0100

    Rearranged files

--- dataProcessing/tweetContentPreprocessing.py
+++ dataProcessing/tweetContentPreprocessing.py
@@ -1,5 +1,5 @@
 import json
-from watson_developer_cloud import NaturalLanguageUnderstandingV1
+#from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EmotionOptions
 
 
